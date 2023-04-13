03483f74d28ae4e5f897805432d380f46c012cfc
v-Alenz <andrea.valenzano97@gmail.com>
2023-04-13 17:48:49 +0200

    added license

--- src/main.c
+++ src/main.c
@@ -1,3 +1,27 @@
+/* PCAD - Pthread Matrix
+ * Prima consegna del corso di PCAD dell'Universita' degli Studi di Genova
+ * Programma per la manipolazione di matrici in parallelo 
+ * creata usando la libreria pthread
+ *
+ * Copyright (C) 2023 Andrea Valenzano 4548315
+ * Copyright (C) 2023 Lorenzo Contino 4832500
+ * Copyright (C) 2023 Eugenio Pallestrini 4878184
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */ 
+
 #include "matrix.h"
 
 
